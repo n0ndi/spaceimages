5c4ff3facb12ed3e67bef6928c8b99eb4cce311e
n0ndi <92996169+n0ndi@users.noreply.github.com>
2022-08-13 08:30:25 +0300

    update format
    -отформатировал код в PEP-8

--- download_image_func.py
+++ download_image_func.py
@@ -1,9 +1,9 @@
 import os
 import requests
 
+
 def download_image(url, path, params=None):
     response = requests.get(url, params)
     response.raise_for_status()
     with open(path, 'wb') as file:
         file.write(response.content)
-
